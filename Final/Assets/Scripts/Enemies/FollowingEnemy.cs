95627255e7a6891decadbdc3dfabaa78ec6f3ff7
Noe235 <sannoemie@outlook.com>
2025-03-17 15:55:54 -0400

    change AI movement to nav mesh and state machine

--- Final/Assets/Scripts/Enemies/FollowingEnemy.cs
+++ Final/Assets/Scripts/Enemies/FollowingEnemy.cs
@@ -1,30 +1,76 @@
 using System;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowingEnemy : MonoBehaviour
 {
-    [SerializeField] private GameObject target;
+    [SerializeField] private GameObject core;
+    [SerializeField] private GameObject player;
     [SerializeField] private float followspeed;
 
     [SerializeField] private float health;
     [SerializeField] private float maxHealth;
     [SerializeField] private EnemyHealth healthBar;
 
+    public NavMeshAgent agent;
+    
+    private enum State {
+        HeadToCore,
+        AttackPlayer,
+    }
+    private State state;
 
     void Awake() {
         healthBar = GetComponentInChildren<EnemyHealth>();
+        state = State.HeadToCore;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Core");
+      core = GameObject.FindGameObjectWithTag("Core");
+      player = GameObject.FindGameObjectWithTag("Player");
+      
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, followspeed * Time.deltaTime);
-        transform.forward = target.transform.position - transform.position;
+        switch (state) {
+            default:
+                case State.HeadToCore:
+                    //Previous movement toward target script
+                   // transform.position = Vector3.MoveTowards(transform.position, target.transform.position, followspeed * Time.deltaTime); //go toward target
+                  //  transform.forward = target.transform.position - transform.position;
+                   agent.SetDestination(core.transform.position); //to work nav mesh surface needs to be put on the surface
+                    FindPlayer();
+                break;
+            
+            case State.AttackPlayer:
+                agent.SetDestination(player.transform.position);
+
+                float attackRange = 10f;
+                
+                //target too far go back to core
+                if (Vector3.Distance(transform.position, player.transform.position) > 18f) {
+                    state = State.HeadToCore;
+                }
+                if (Vector3.Distance(transform.position, player.transform.position) < attackRange) {
+                    //within attack range
+                    //in prevention of the attaack being on each frames 
+                   // if (Time.time > nextshootTime){
+                   //float firerate = enemy.attackspeed
+                   //nexshootTime = Time.time + firerate
+                   // }
+                   //
+                    //insert attack animation or like script
+                }
+
+                break;
+        }
+      
+        
     }
 
     private void OnCollisionEnter(Collision other) {
@@ -41,6 +87,15 @@ public class FollowingEnemy : MonoBehaviour
         healthBar.UpdateHealthBar(health,maxHealth);
         if (health <= 0) {
             Destroy(gameObject);
+        }
+    }
+
+    private void FindPlayer() {
+        float playerDetectRange = 15f;
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (Vector3.Distance(transform.position, player.transform.position) < playerDetectRange) {
+            //if near the player range change state
+            state = State.AttackPlayer;
         }
     }
 }
--- Final/Assets/Scripts/Player/FPSShooter.cs
+++ Final/Assets/Scripts/Player/FPSShooter.cs
@@ -15,6 +15,8 @@ public class FPSShooter : MonoBehaviour
     private bool leftHand;
     private float timeToFire;
     
+   
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         flamethrower.Stop();
