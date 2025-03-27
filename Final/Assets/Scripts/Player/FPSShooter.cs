f00f5a7a13f30518a769309da7f31a2f3b60baf4
Noe235 <sannoemie@outlook.com>
2025-03-27 11:24:15 -0400

    Finished adding Ice spell

--- Final/Assets/Scripts/Player/FPSShooter.cs
+++ Final/Assets/Scripts/Player/FPSShooter.cs
@@ -1,68 +1,98 @@
+using System.Runtime.ExceptionServices;
 using UnityEngine;
 using UnityEngine.VFX;
 
-public class FPSShooter : MonoBehaviour
-{
+public class FPSShooter : MonoBehaviour {
     public Camera cam;
     public VisualEffect flamethrower;
-    public GameObject projectile;
-    public Transform LHFirePoint, RHFirePoint;
+    public GameObject fireball;
+    public GameObject ice;
+    public Transform LHFirePoint, RHFirePoint, IceFirePoint1, IceFirePoint2, IceFirePoint3, IceFirePoint4, IceFirePoint5;
     public float projectileSpeed = 30;
     public float fireRate = 4;
-
+    public string spell;
     static public int spellLevel = 1;
     
-    
+
     private Vector3 destination;
     private bool leftHand;
     private float timeToFire;
-    
-   
-    
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         flamethrower.Stop();
+        spell = "Fire";
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.H)) {
+            if (spell == "Ice") {
+                spell = "Fire";
+            } else if (spell == "Fire") {
+                spell = "Ice";
+            }
+            Debug.Log("Key down");
+        }
         if (gameObject.GetComponent<FPSController>().mouseOff) {
-            if (Input.GetButton("Fire1") && Time.time >= timeToFire) {
-                timeToFire = Time.time + 1f / fireRate;
-                ShootProjectile();
-            }
+                if (Input.GetButton("Fire1") && Time.time >= timeToFire) {
+                    timeToFire = Time.time + 1f / fireRate;
+                    ShootProjectile();
+                }
 
-            if (Input.GetButtonDown("Fire2")) {
-                flamethrower.Play();
-            }
+                if (Input.GetButtonDown("Fire2")) {
+                    flamethrower.Play();
+                }
 
-            if (Input.GetButtonUp("Fire2")) {
-                flamethrower.Stop();
-            }
+                if (Input.GetButtonUp("Fire2")) {
+                    flamethrower.Stop();
+                }
+                
         }
     }
-    void ShootProjectile() {
-       Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-       RaycastHit hit;
-       
-       if (Physics.Raycast(ray, out hit)) 
-           destination = hit.point;
-       else 
-           destination = ray.GetPoint(1000);
 
-       if (leftHand) {
-           leftHand = false;
-           InstantiateProjectile(LHFirePoint);
+    void ShootProjectile() {
+       
+       if (spell == "Fire") {
+       /*    Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+            destination = hit.point;
+        else
+            destination = ray.GetPoint(1000);
+        */
+           if (leftHand) {
+               leftHand = false;
+               InstantiateProjectile(LHFirePoint);
+           }
+           else {
+               leftHand = true;
+               InstantiateProjectile(RHFirePoint);
+           }
        }
-       else {
-           leftHand = true;
-           InstantiateProjectile(RHFirePoint);
+
+       if (spell == "Ice") { //probably make an array ._.
+           ShootIce(IceFirePoint1);
+           ShootIce(IceFirePoint2);
+           ShootIce(IceFirePoint3);
+           ShootIce(IceFirePoint4);
+           ShootIce(IceFirePoint5);
        }
     }
 
     void InstantiateProjectile(Transform firePoint) {
-        GameObject projectileObj = Instantiate (projectile, firePoint.position, Quaternion.identity);
-      projectileObj.GetComponent<Rigidbody>().AddForce(firePoint.forward * projectileSpeed, ForceMode.Impulse);
+        GameObject projectileObj = Instantiate(fireball, firePoint.position, Quaternion.identity);
+        projectileObj.GetComponent<Rigidbody>().AddForce(firePoint.forward * projectileSpeed, ForceMode.Impulse);
     }
+
+    void ShootIce(Transform firePoint) {
+        GameObject projectileObj = Instantiate(ice, firePoint.position, Quaternion.identity);
+        projectileObj.GetComponent<Rigidbody>().AddForce(firePoint.forward * projectileSpeed, ForceMode.Impulse); 
+    }
+
+
 }
